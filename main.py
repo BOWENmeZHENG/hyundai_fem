5296fe38eb54539c1515d02217d639e06a043c6f
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-09-22 10:46:17 -0700

    get fem in batch

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import numpy as np
 import os
 import shutil
 
-radii = np.linspace(0.05, 0.15, 4)
-centers_x = np.linspace(-0.5, 0.5, 4)
-centers_y = np.linspace(-0.075, 0.075, 4)
+radii = np.linspace(0.05, 0.1, 4)
+centers_x = np.linspace(-0.1, 0.1, 4)
+centers_y = np.linspace(-0.05, 0.05, 4)
 
 for radius in radii:
     for center_x in centers_x:
@@ -14,7 +14,8 @@ for radius in radii:
                                   radius=radius, center_x=center_x, center_y=center_y,
                                   E=1.0e8, nu=0.3, load=20.0e3
                                   )
-            os.makedirs("data", exist_ok=True)
-            shutil.copyfile(f"{folder}/{folder}_elements.csv", f"data/{folder}_elements.csv")
-            shutil.copyfile(f"{folder}/{folder}_nodes.csv", f"data/{folder}_nodes.csv")
+            data_folder = "data2"
+            os.makedirs(data_folder, exist_ok=True)
+            shutil.copyfile(f"{folder}/{folder}_elements.csv", f"{data_folder}/{folder}_elements.csv")
+            shutil.copyfile(f"{folder}/{folder}_nodes.csv", f"{data_folder}/{folder}_nodes.csv")
 
