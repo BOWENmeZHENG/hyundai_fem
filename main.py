97c02f999151dd57d125be1d5aecf817d235db83
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-09-22 10:52:38 -0700

    remove excessive folders

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import numpy as np
 import os
 import shutil
 
-radii = np.linspace(0.05, 0.1, 4)
-centers_x = np.linspace(-0.1, 0.1, 4)
-centers_y = np.linspace(-0.05, 0.05, 4)
+radii = np.linspace(0.05, 0.1, 2)
+centers_x = np.linspace(-0.1, 0.1, 2)
+centers_y = np.linspace(-0.05, 0.05, 2)
 
 for radius in radii:
     for center_x in centers_x:
@@ -14,8 +14,9 @@ for radius in radii:
                                   radius=radius, center_x=center_x, center_y=center_y,
                                   E=1.0e8, nu=0.3, load=20.0e3
                                   )
-            data_folder = "data2"
+            data_folder = "try"
             os.makedirs(data_folder, exist_ok=True)
             shutil.copyfile(f"{folder}/{folder}_elements.csv", f"{data_folder}/{folder}_elements.csv")
             shutil.copyfile(f"{folder}/{folder}_nodes.csv", f"{data_folder}/{folder}_nodes.csv")
+            shutil.rmtree(folder)
 
